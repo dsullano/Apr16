779b7ae46e421aca08989024489ccc78d05b148e
LAB14_USER ON WS06 <L14Y15W06@citulab.net>
2024-04-16 15:54:17 +0800

    added sql

--- src/MySQLConnection.java
+++ src/MySQLConnection.java
@@ -1,16 +1,29 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class MySQLConnection {
     public static final String URL = "jdbc:mysql://localhost:3306/dbcsit228sullano";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "";
     static Connection getConnection() {
 
         Connection c = null;
 
         try{
-            c = DriverManager.getConnection();
-        }catch{}
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+            System.out.println("Database Connection Success");
+        }catch(SQLException e){
+            e.printStackTrace();
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
 
         return c;
+    }
+
+    public static void main(String[] args) {
+        getConnection();
     }
 }
